2e20a7457897638e1b1fe1de990484339fd09c3a
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2021-05-10 04:26:52 -0500

    Flujo de reportar mascota

--- HttpTriggerMascotaReportar/__init__.py
+++ HttpTriggerMascotaReportar/__init__.py
@@ -25,26 +25,25 @@ block_blob_service = BlockBlobService(
     account_key=ACCOUNT_KEY
 )
 
-def reportar_mascota_desaparecida(image_bytes):
+def reportar_mascota_desaparecida(nombre_imagen):
     '''
     Invocar API para registrar en memoria la nueva imagen del rostro de la mascota
     '''
     logging.info('reportar_mascota_desaparecida')
     try:
         if ENDPOINT_REPORTAR_MASCOTA:
-            files = {'upload_file': image_bytes}
+            file_imagen = open(nombre_imagen,'rb')
+            files = {'upload_file': file_imagen}
             response = requests.post(ENDPOINT_REPORTAR_MASCOTA, files=files)
-
             logging.info('Respuesta: {}'.format(response.text))
-            respuesta = json.loads(response.text)['imagen']
+            respuesta = json.loads(response.text)
             
-            if type(respuesta) == bytes:
-                return True, respuesta
-            else:
-                return False, None
+            file_imagen.close()
+            return True, respuesta
         return False, 'No se ha inicializado variable de entorno ENDPOINT_REPORTAR_MASCOTA'
     except Exception as e:
         logging.info('Error al reportar mascota: {}'.format(e))
+        file_imagen.close()
         return False, None
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -59,7 +58,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         logging.info(data)
         
         #
-        # Guardar imagen en Azure Storage
+        # Crear archivo temporal
         #
         current_date = datetime.utcnow().strftime('%Y-%m-%d_%H%M%S.%f')[:-3]
         nombre_imagen = 'image_{}.jpg'.format(current_date)
@@ -70,16 +69,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
         with open(file_path, 'wb') as f:
             f.write(data.read())
-
-        blob_name = f"images/{nombre_imagen}"
-        logging.info(blob_name)
-        logging.info(file_path)
-        block_blob_service.create_blob_from_path(CONTAINER_NAME, blob_name, file_path)
-
+        
         #
         # Registrar en memoria la imagen reportada
         #
-        flag, respuesta = reportar_mascota_desaparecida(data)
+        flag, dict_respuesta = reportar_mascota_desaparecida(file_path)
+        ## Respuesta:
+        # dict_respuesta['file_name']
+        # dict_respuesta['label']
 
         if not flag:
             dict_respuesta['mensaje'] = "Hubo un error. Volver a ingresar la imagen."
@@ -89,15 +86,25 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 status_code=400
             )
 
-        #    #
-        #    # Guardar en base de datos
-        #    #
-        #    #Genero un valor random para la distancia
-        distancia = str(random.randint(0,999))+' km.'
-        file_name = 'static\\1\\1.21.jpg'
-        label = '1'
+        #
+        # Guardar imagen en Azure Storage
+        #
+        blob_name = f"images/{nombre_imagen}"
+        logging.info(blob_name)
+        logging.info(file_path)
+        block_blob_service.create_blob_from_path(CONTAINER_NAME, blob_name, file_path)
 
-        flag, respuesta = mongodb.registrar_mascota_reportada(encoded_string=imagen_base, image_path=file_name, label, caracteristicas, distancia)
+        #
+        # Guardar en base de datos
+        #
+        # Genero un valor random para la distancia
+        distancia = str(random.randint(0,999))+' km.'
+        file_name = dict_respuesta['file_name']
+        label = dict_respuesta['label']
+        with open(file_path, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+
+        flag, respuesta = mongodb.registrar_mascota_reportada(encoded_string=encoded_string, image_path=file_name, label, caracteristicas, distancia)
         if not flag:
             dict_respuesta['mensaje'] = "Hubo un error. Volver a ingresar la imagen."
             return func.HttpResponse(
